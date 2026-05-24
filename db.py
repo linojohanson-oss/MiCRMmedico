ea5f3870b580ccedf231337ae1f864b2621986a1
linojohanson-oss <linojohanson@gmail.com>
2026-05-24 16:30:26 -0300

    Connect Flask to Railway DB

--- db.py
+++ db.py
@@ -1,15 +1,19 @@
+import os
 import pymysql
 
 def obtener_conexion(dict_cursor=False):
     try:
         cursor_type = pymysql.cursors.DictCursor if dict_cursor else pymysql.cursors.Cursor
+
         return pymysql.connect(
-            host='localhost',
-            user='root',
-            password='',
-            database='mi_crm',
+            host=os.getenv("MYSQLHOST"),
+            user=os.getenv("MYSQLUSER"),
+            password=os.getenv("MYSQLPASSWORD"),
+            database=os.getenv("MYSQLDATABASE"),
+            port=int(os.getenv("MYSQLPORT")),
             cursorclass=cursor_type
         )
+
     except Exception as e:
         print("Error de conexion:", e)
         return None
