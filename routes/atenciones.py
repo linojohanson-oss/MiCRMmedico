00955897560637dd00559fe8f28c1f88a66c3f95
linojohanson-oss <linojohanson@gmail.com>
2026-05-25 15:53:20 -0300

    Quita presion arterial clinica

--- routes/atenciones.py
+++ routes/atenciones.py
@@ -93,7 +93,6 @@ def registrar_atencion(cita_id):
                 notas_clinica,
                 tipo_diabetes,
                 hba1c,
-                presion_arterial_clinica,
                 notas_extra
             )
             VALUES (
@@ -120,7 +119,6 @@ def registrar_atencion(cita_id):
             notas_clinica,
             tipo_diabetes,
             hba1c,
-            presion_arterial_clinica,
             notas_extra_json
         ))
 
