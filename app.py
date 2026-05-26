74d816ff80160f08a13f41c9808f2c64c7e8c50e
linojohanson-oss <linojohanson@gmail.com>
2026-05-26 19:15:42 -0300

    Registra blueprint reportes

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from routes.clientes import clientes_bp
 from routes.doctores import doctores_bp
 from routes.citas import citas_bp
 from routes.atenciones import atenciones_bp
+from routes.reportes import reportes_bp
 
 app = Flask(__name__)
 app.secret_key = 'clave_secreta'
@@ -13,6 +14,7 @@ app.register_blueprint(clientes_bp)
 app.register_blueprint(doctores_bp)
 app.register_blueprint(citas_bp)
 app.register_blueprint(atenciones_bp)
+app.register_blueprint(reportes_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
